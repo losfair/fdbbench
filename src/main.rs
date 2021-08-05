ca9a93f98968f74e32da578aaed5b4d19d5f521c
losfair <zhy20000919@hotmail.com>
2021-08-05 20:39:31 +0800

    Optimize joining

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+  iter::FromIterator,
   sync::{
     atomic::{AtomicUsize, Ordering},
     Arc,
@@ -9,6 +10,7 @@ use std::{
 use anyhow::Result;
 use foundationdb::{tuple::Subspace, Database};
 
+use futures::{stream::FuturesUnordered, StreamExt};
 use rand::Rng;
 use structopt::StructOpt;
 
@@ -106,63 +108,68 @@ async fn async_main() -> Result<()> {
       });
       futures.push(fut);
     }
-    futures::future::join_all(futures).await;
+    FuturesUnordered::from_iter(futures.into_iter())
+      .for_each(|_| async {})
+      .await;
   }
   log::info!("Starting benchmark.");
   let start_time = Instant::now();
 
   let stats = Arc::new(Stats::default());
 
-  let tasks = (0..opt.concurrency).map(|_| {
-    let db = db.clone();
-    let iterations = opt.iterations;
-    let reads = opt.reads;
-    let writes = opt.writes;
-    let min_value_size = opt.min_value_size;
-    let max_value_size = opt.max_value_size;
-    let subspace = subspace.clone();
-    let key_pool = opt.key_pool;
-    let snapshot = opt.snapshot;
-    let stats = stats.clone();
-    tokio::spawn(async move {
-      for _ in 0..iterations {
-        loop {
-          let txn = db.create_trx().unwrap();
-          for _ in 0..reads {
-            let x = rand::thread_rng().gen_range(0..key_pool);
-            let key = subspace.pack(&(x as u64));
-            let _ = txn
-              .get(&key, snapshot)
-              .await
-              .unwrap()
-              .expect("missing value for key");
-          }
-          stats.num_reads.fetch_add(reads, Ordering::Relaxed);
-
-          for _ in 0..writes {
-            let x = rand::thread_rng().gen_range(0..key_pool);
-            let key = subspace.pack(&(x as u64));
-            let len = rand::thread_rng().gen_range(min_value_size..=max_value_size);
-            let mut buf = vec![0u8; len];
-            rand::thread_rng().fill(buf.as_mut_slice());
-            txn.set(&key, &buf);
-          }
-
-          if let Err(e) = txn.commit().await {
-            if e.is_retryable() {
-              stats.num_retries.fetch_add(1, Ordering::Relaxed);
-              continue;
+  (0..opt.concurrency)
+    .map(|_| {
+      let db = db.clone();
+      let iterations = opt.iterations;
+      let reads = opt.reads;
+      let writes = opt.writes;
+      let min_value_size = opt.min_value_size;
+      let max_value_size = opt.max_value_size;
+      let subspace = subspace.clone();
+      let key_pool = opt.key_pool;
+      let snapshot = opt.snapshot;
+      let stats = stats.clone();
+      tokio::spawn(async move {
+        for _ in 0..iterations {
+          loop {
+            let txn = db.create_trx().unwrap();
+            for _ in 0..reads {
+              let x = rand::thread_rng().gen_range(0..key_pool);
+              let key = subspace.pack(&(x as u64));
+              let _ = txn
+                .get(&key, snapshot)
+                .await
+                .unwrap()
+                .expect("missing value for key");
             }
-            panic!("commit error");
+            stats.num_reads.fetch_add(reads, Ordering::Relaxed);
+
+            for _ in 0..writes {
+              let x = rand::thread_rng().gen_range(0..key_pool);
+              let key = subspace.pack(&(x as u64));
+              let len = rand::thread_rng().gen_range(min_value_size..=max_value_size);
+              let mut buf = vec![0u8; len];
+              rand::thread_rng().fill(buf.as_mut_slice());
+              txn.set(&key, &buf);
+            }
+
+            if let Err(e) = txn.commit().await {
+              if e.is_retryable() {
+                stats.num_retries.fetch_add(1, Ordering::Relaxed);
+                continue;
+              }
+              panic!("commit error");
+            }
+            stats.num_txns.fetch_add(1, Ordering::Relaxed);
+            stats.num_writes.fetch_add(writes, Ordering::Relaxed);
+            break;
           }
-          stats.num_txns.fetch_add(1, Ordering::Relaxed);
-          stats.num_writes.fetch_add(writes, Ordering::Relaxed);
-          break;
         }
-      }
+      })
     })
-  });
-  futures::future::join_all(tasks).await;
+    .collect::<FuturesUnordered<_>>()
+    .for_each(|_| async {})
+    .await;
   let end_time = Instant::now();
   println!("{:?}", stats);
   println!("{:?}", end_time.duration_since(start_time));
